2780c9a9086188d9f79681b90c3f7e3b7a807315
Sushil Nayak <sushil.nayak@gmail.com>
2024-04-11 08:52:48 +0530

    initial

---
+++ src/main/java/com/nayak/App.java
@@ -0,0 +1,17 @@
+package com.nayak;
+
+import java.math.BigDecimal;
+
+public class App {
+    static BigDecimal getNumber() {
+        return BigDecimal.valueOf(0);
+    }
+
+    public boolean compare(String firstList[], String secondList[]) {
+        return firstList.equals(secondList);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
