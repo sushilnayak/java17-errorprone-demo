6f445e8f55c05d2aeec7e98111ad827788421203
Sushil Nayak <sushil.nayak@gmail.com>
2024-04-11 09:27:11 +0530

    added refaster

--- src/main/java/com/nayak/App.java
+++ src/main/java/com/nayak/App.java
@@ -6,7 +6,6 @@ public class App {
     static BigDecimal getNumber() {
         return BigDecimal.valueOf(0);
     }
-
     public boolean compare(String firstList[], String secondList[]) {
         return firstList.equals(secondList);
     }
